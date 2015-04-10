e711ae02ee7b609107e9d8cb90a1e386715ed194
learrsan <gaslas@hotmail.com>
2015-04-10 15:56:16 +0000

    10/04/2015 15:56

---
+++ TareasListasJavaScript/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6198ccf6-2214-46cc-86a7-c2ec32f1ff3f" description="SharePoint App Feature" featureId="6198ccf6-2214-46cc-86a7-c2ec32f1ff3f" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="TareasListasJavaScript Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7e275459-b5be-466c-9c98-5b8608d080ee" />
+    <projectItemReference itemId="3ef4de61-dee4-458f-90d0-4c953534f4c1" />
+    <projectItemReference itemId="dc6f5ec8-b880-49ce-a745-ecc05afbce46" />
+    <projectItemReference itemId="72bedce1-d5f6-411d-b124-e492a4f4bd08" />
+    <projectItemReference itemId="e2dd65d5-5ad7-40e3-ac82-71db7534e387" />
+    <projectItemReference itemId="caec6af4-63a6-47b8-b952-02e6c4fc0d45" />
+  </projectItems>
+</feature>
---
+++ TareasListasJavaScript/Scripts/App.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+
+
+var MisListas = Window.MisListas || {};
+
+MisListas.CargarItem = function() {
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+    var obtenerElementosUsandoLoad = function() {
+        var lista = context.get_web().get_lists.getByTitle["Tareas"];
+        context.load(lista);
+        var items = lista.getItems('');
+        context.load(items);
+
+        context.executeQueryAsync(
+            function() {
+                var enumeracion = items.getEnumerator();
+
+                while (enumeracion.moveNext()) {
+                    var item = enumeracion.getCurrent();
+                    $("#lista").append("<li>" + item.get_item("Title") + "</li>");
+                }
+
+            },
+            function() {
+                alert("Error al acceder a los datos");   
+            }
+        
+    );
+};
+};
+
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    getUserName();
+});
+
+// This function prepares, loads, and then executes a SharePoint query to get the current users information
+function getUserName() {
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+// This function is executed if the above call is successful
+// It replaces the contents of the 'message' element with the user name
+function onGetUserNameSuccess() {
+    $('#message').text('Hello ' + user.get_title());
+}
+
+// This function is executed if the above call fails
+function onGetUserNameFail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
